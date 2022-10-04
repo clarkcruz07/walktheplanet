6953e3282479f953125a5dc47bfa02913cc0fb7e
clarkcruz07 <clark.cruz07@gmail.com>
2022-10-04 17:45:11 +0800

    pushing changes

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import VideoPlayer from "react-background-video-player";
+import ReactAudioPlayer from 'react-audio-player';
 const ComingSoon = () =>{
+    const src = "/homepage.mp4";
+    const audiosrc ="/bg.ogg";
+    const fontStyle = "/comingsoon-01.svg"
+
     return (
         <div className="video-content">
+            <VideoPlayer
+                className="video"
+                src={src}  autoPlay={true}
+                muted={true}
+            />
+            <ReactAudioPlayer
+                src={audiosrc}
+                autoPlay={true}
+            />
             <div className="app-wrapper">
-                <h4 className="app-header">Walk the Planet</h4>
-                <h1 className="app-content">COMING SOON</h1>
+                <img src={fontStyle} alt="Walk the Planet Image" />
             </div>
         </div>
     )
